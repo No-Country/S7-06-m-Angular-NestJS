05aaad4b59b1d0b13b090707846a3ebed0286728
Santiago <monitocastellani@gmail.com>
2023-03-31 16:42:39 -0300

    feat:use nav and footer common

--- frontend/src/app/private/cart/cart.module.ts
+++ frontend/src/app/private/cart/cart.module.ts
@@ -4,16 +4,18 @@ import { CartProductsComponent } from './components/cart-products/cart-products.
 import { CartComponent } from './page/cart/cart.component';
 import { CartRoutingModule } from './cart-routing.module';
 import { SelectorComponent } from './components/selector/selector.component';
+import { SharedModule } from "../../shared/shared.module";
 
 @NgModule({
-  declarations: [
-    CartProductsComponent,
-    CartComponent,
-    SelectorComponent
-  ],
-  imports: [
-    CommonModule,
-    CartRoutingModule
-  ]
+    declarations: [
+        CartProductsComponent,
+        CartComponent,
+        SelectorComponent
+    ],
+    imports: [
+        CommonModule,
+        CartRoutingModule,
+        SharedModule
+    ]
 })
 export class CartModule { }
