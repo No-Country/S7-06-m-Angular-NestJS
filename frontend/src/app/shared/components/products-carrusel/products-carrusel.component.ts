82a9415fe04a279fd59d661f3cacfe38b2ef2245
Federico Di Cillo <dicillo.fe@gmail.com>
2023-04-06 11:55:46 -0300

    Responsive Home from width 1700px

--- frontend/src/app/shared/components/products-carrusel/products-carrusel.component.ts
+++ frontend/src/app/shared/components/products-carrusel/products-carrusel.component.ts
@@ -22,10 +22,10 @@ export class ProductsCarruselComponent implements OnInit {
   config: SwiperOptions = {
   slidesPerView: 1,
   spaceBetween: 10,
-  autoplay: { delay: 5000, disableOnInteraction: false},
+  //autoplay: { delay: 5000, disableOnInteraction: false},
   loop: true,
   navigation: false,
-  scrollbar:true,
+  pagination:true,
   breakpoints: {
     500: {
       slidesPerView:4,
@@ -36,24 +36,24 @@ export class ProductsCarruselComponent implements OnInit {
   
   products: Product[] = [
     {id:1,
-    name:'uno',
+    name:'Gomas de colores',
     price:123,
     category:'uno',
-    imageUrl:'../../../../../assets/store/products/Lapiceras 1100.png',
+    imageUrl:'https://http2.mlstatic.com/D_NQ_NP_865110-MLA54031227294_022023-W.jpg',
     description:'un producto,'
     },
     {id:2,
-      name:'dos',
+      name:'Lapiceras',
       price:123,
       category:'dos',
       imageUrl:'../../../../../assets/store/products/Lapiceras 1100.png',
       description:'otro producto,'
     },
     {id:3,
-      name:'tres',
+      name:'Notas adesivas',
       price:123,
       category:'tres',
-      imageUrl:'../../../../../assets/store/products/Lapiceras 1100.png',
+      imageUrl:'https://http2.mlstatic.com/D_NQ_NP_712764-MLA50970012056_082022-O.jpg',
       description:'y otro producto,'
     },
     {id:4,
