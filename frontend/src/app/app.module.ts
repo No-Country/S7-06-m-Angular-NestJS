4ef8e864206a6a176de3ca69766b6c86376c3593
Sharon <ormachea.sharon@gmail.com>
2023-04-12 04:02:20 -0300

    Implementacion de pagina de carga en la app web

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,14 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { PublicModule } from './public/public.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PrivateModule } from './private/private.module';
-import { interceptorProvider } from './shared/interceptors/interceptor.service';
+import { interceptorProvider } from './shared/interceptors/interceptor/interceptor.service';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from './shared/shared.module';
 import { PublicRoutingModule } from './public/public-routing.module';
+import { LoadingInterceptor } from './shared/interceptors/loading/loading.interceptor';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,10 @@ import { PublicRoutingModule } from './public/public-routing.module';
     PrivateModule,
     BrowserAnimationsModule
   ],
-  providers: [interceptorProvider],
+  providers: [
+    interceptorProvider,
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
