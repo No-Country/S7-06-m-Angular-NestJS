b0f01d8cdb23cc3e7de9ac0918a731f23283b2a0
Bryansss1 <gregoria1540@gmail.com>
2023-04-12 12:17:04 -0400

    cambios en los servicios orders y swagger

--- backend/src/orders/orders.module.ts
+++ backend/src/orders/orders.module.ts
@@ -5,10 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
 import { OrderItem } from 'src/orders/entities/order-item.entity';
 import { Product } from 'src/products/entities';
+import { User } from 'src/auth/entities/auth.entity';
 
 @Module({
   controllers: [OrdersController],
   providers: [OrdersService],
-  imports: [TypeOrmModule.forFeature([Order, OrderItem, Product])],
+  imports: [TypeOrmModule.forFeature([Order, OrderItem, Product,User])],
 })
 export class OrdersModule {}
