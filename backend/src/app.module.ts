b2cd026703c1819203c5bd3dd76618ce64e14f2e
Josue Ordonez <86974285+r4zu@users.noreply.github.com>
2023-04-17 08:01:41 -0500

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -23,7 +23,7 @@ import { OrdersModule } from './orders/orders.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
-      synchronize: false,
+      synchronize: true,
     }),
     AuthModule,
     ProductsModule,
